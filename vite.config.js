66f1865ab8060d6c910869fd7f8548c3a7dfae3c
ryiianqueiroz <ryanqueirozdev44@gmail.com>
2024-08-06 00:44:40 -0300

    v1.21

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173,
     proxy: {
-      '/api': 'http://localhost:5000' // Proxy API requests to Express server
-    }
-  }
-})
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+});
