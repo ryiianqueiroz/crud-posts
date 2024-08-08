b112b4010e76e544a5d1e924fb4d52843ad918a1
ryiianqueiroz <ryanqueirozdev44@gmail.com>
2024-08-08 15:51:55 -0300

    vercel1.26 error

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 5173, // Porta para desenvolvimento local
   },
 });
