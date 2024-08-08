200c55033896d1e4b0fd8e54383a5ab53dcf6277
ryiianqueiroz <ryanqueirozdev44@gmail.com>
2024-08-08 14:44:33 -0300

    v1.24

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+// eslint-disable-next-line no-unused-vars
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    // Configuração de desenvolvimento
+    return {
+      plugins: [react()],
+      server: {
+        port: 5173,
+        proxy: {
+          '/api': {
+            target: 'http://localhost:5000',
+            changeOrigin: true,
+          },
+        },
       },
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
+    };
+  } else {
+    // Configuração de produção
+    return {
+      plugins: [react()],
+      build: {
+        outDir: 'dist',
+      },
+      server: {
+        // eslint-disable-next-line no-undef
+        port: process.env.PORT || 3000, // Use a porta definida pelo Railway ou a 3000 como fallback
+      },
+    };
+  }
 });
